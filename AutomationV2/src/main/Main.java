1078638473983c2bd810e2b7350f9a884c7766c1
Valentin <hamilcare@gmail.com>
2019-11-23 15:41:02 +0100

    Fix translator

--- AutomationV2/src/main/Main.java
+++ AutomationV2/src/main/Main.java
@@ -5,12 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-import aggregators.impl.Sum;
+import aggregators.impl.Max;
 import alphabet.Alphabet;
 import automaton.Automaton;
 import automaton.AutomatonBuilder;
-import features.impl.One;
+import features.impl.Width;
 import translation.ITranslator;
 import translation.Translate;
 
@@ -18,19 +19,27 @@ public class Main {
 
 	static String path = "input/peak.csv";
 
-	static String input = "input/pi100000digit";
+	static String input = "input/100000000.digt";
+//	static String input = "input/exemple";
+
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) throws IOException {
 
 		ITranslator translator = translateInput(input);
 		Automaton.AUTOMATON.setInputSequenceLenght(translator.getTextToTranslate().size());
 
-		AutomatonBuilder builder = new AutomatonBuilder(path, new One(), new Sum());
+		AutomatonBuilder builder = new AutomatonBuilder(path, new Width(), new Max());
 		builder.build();
 
 //		System.out.println(translator.getTranslatedText());
 
 		long endTransaltion = System.currentTimeMillis();
+
+		System.out.println("Waiting input before computation");
+		sc.nextLine();
+
+		long startComputation = System.currentTimeMillis();
 
 		for (int i = 0; i < translator.getTranslatedText().length(); i++) {
 			Automaton.AUTOMATON.applyNextInput(Alphabet.asEnum("" + translator.getTranslatedText().charAt(i)));
@@ -40,7 +49,7 @@ public class Main {
 
 		System.out.println(Automaton.AUTOMATON.getResult());
 
-		System.out.println("Time : " + (endComputation - endTransaltion));
+		System.out.println("Time : " + (endComputation - startComputation));
 	}
 
 	public static ITranslator translateInput(String pathToData) {
--- AutomationV2/src/translation/Translate.java
+++ AutomationV2/src/translation/Translate.java
@@ -8,16 +8,16 @@ import alphabet.Alphabet;
 public class Translate implements ITranslator {
 
 	List<Integer> textToTranslate;
-	StringBuffer translatedText;
+	String translatedText;
 
 	public Translate() {
 		textToTranslate = new ArrayList<>();
-		translatedText = new StringBuffer();
+
 	}
 
 	public Translate(List<Integer> textToTranslate) {
 		this.textToTranslate = textToTranslate;
-		translatedText = new StringBuffer(textToTranslate.size());
+
 	}
 
 	@Override
@@ -27,23 +27,25 @@ public class Translate implements ITranslator {
 
 	@Override
 	public String getTranslatedText() {
-		return translatedText.toString();
+		return translatedText;
 
 	}
 
 	@Override
 	public void translate() {
+		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < textToTranslate.size() - 1; i++) {
 			Integer currentChar = textToTranslate.get(i);
 			Integer nextChar = textToTranslate.get(i + 1);
 			if (currentChar < nextChar) {
-				translatedText.append(Alphabet.MORE.asString());
+				sb.append(Alphabet.MORE.asString());
 			} else if (currentChar > nextChar) {
-				translatedText.append(Alphabet.LESS.asString());
+				sb.append(Alphabet.LESS.asString());
 			} else {
-				translatedText.append(Alphabet.EQUALS.asString());
+				sb.append(Alphabet.EQUALS.asString());
 			}
 		}
+		translatedText = sb.toString();
 	}
 
 }
